dba30ae26e1d36d1eecfcae1efd9127848b12e69
yvancompaore <yvan.compaore@edu.ece.fr>
2019-06-07 12:57:15 +0200

    Mise en place du Jtable et du deplacement

--- vue/GAnnee.java
+++ vue/GAnnee.java
@@ -12,12 +12,19 @@ import controleur.AnneescolaireDAO;
 import model.Anneescolaire;
 import model.Connexion;
 import net.proteanit.sql.DbUtils;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class GAnnee extends JFrame{
     private AnneescolaireDAO Annee;
     private Connexion con;
     private JTable table;
-	
+    private JTextField textFieldId;
+    private JTextField textFieldAnnee;
+	private static int test;
+    
+    
+    
 	public GAnnee(Connexion con) {
 		super("Gestion Annee");
 		getContentPane().setForeground(Color.BLACK);
@@ -28,18 +35,53 @@ public class GAnnee extends JFrame{
 		getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 480, 322);
+		panel.setBounds(298, 0, 536, 221);
 		getContentPane().add(panel);
 		panel.setLayout(null);
 		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(31, 62, 484, 138);
+		panel.add(scrollPane);
+		
 		table = new JTable();
-		table.setBounds(140, 68, 225, 48);
-		panel.add(table);
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Deplace();
+			}
+		});
+		scrollPane.setViewportView(table);
 		
 		JButton btnAfficher = new JButton("Afficher");
 		btnAfficher.setBounds(198, 21, 141, 35);
 		btnAfficher.addActionListener(new BtAfficherListener());
 		panel.add(btnAfficher);
+		
+		JLabel lblNewLabel = new JLabel("ID");
+		lblNewLabel.setBounds(18, 102, 92, 26);
+		getContentPane().add(lblNewLabel);
+		
+		textFieldId = new JTextField();
+		textFieldId.setBounds(131, 99, 146, 32);
+		getContentPane().add(textFieldId);
+		textFieldId.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Annee");
+		lblNewLabel_1.setBounds(21, 155, 92, 26);
+		getContentPane().add(lblNewLabel_1);
+		
+		textFieldAnnee = new JTextField();
+		textFieldAnnee.setBounds(131, 152, 146, 32);
+		getContentPane().add(textFieldAnnee);
+		textFieldAnnee.setColumns(10);
+		
+		JLabel lblInfos = new JLabel("INFOS");
+		lblInfos.setBounds(109, 55, 92, 26);
+		getContentPane().add(lblInfos);
+		
+		JLabel lblFond = new JLabel("");
+		lblFond.setBounds(0, 0, 834, 600);
+		getContentPane().add(lblFond);
 		this.con=con;
 		
 		Annee= new AnneescolaireDAO(con);
@@ -52,6 +94,27 @@ public class GAnnee extends JFrame{
 		this.setVisible(true);
 	}
 	
+	//deplacer text dans case
+	public void Deplace() {
+		try {
+			//on recupere la ligne selectionne
+			int row =table.getSelectedRow();
+			this.test=(int) (table.getModel().getValueAt(row, 0));
+			AnneescolaireDAO annee1 = new AnneescolaireDAO(con);
+			Anneescolaire a=annee1.find(test);
+			
+			textFieldId.setText(String.valueOf(a.getId()));
+			textFieldAnnee.setText(String.valueOf(a.getAnnee()));
+					
+		}catch(Exception e) {
+			e.getMessage();
+		}
+		
+	}
+	
+	
+	
+	
 	class BtAfficherListener implements ActionListener {
 
 		@Override
@@ -60,9 +123,8 @@ public class GAnnee extends JFrame{
 			ResultSet rs =annee1.Ro();
 			System.out.println("ok");
 			table.setModel(DbUtils.resultSetToTableModel(rs));
+			
 		}
 		
 	}
-	
-	
 }
