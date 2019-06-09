b647611dcca9f783866db9cc95e2aa31e7fffc65
Marc Gemayel <marcgemayel@gmail.com>
2019-06-09 22:57:06 +0200

    update

--- vue/Test.java
+++ vue/Test.java
@@ -19,19 +19,7 @@ public class Test {
 			
 			
 
-			//GBulletin g= new GBulletin(con);
 			
-			//GAffichageBulletin a = new GAffichageBulletin(con,2);
-			//GProf g= new GProf(con);
-			//GDiscipline g = new GDiscipline(con);
-			GEvaluation g=new GEvaluation(con);
-			//GInscription g=new GInscription(con);
-			//GEleve g = new GEleve(con);
-			
-
-			//GGraphique g=new GGraphique(con);
-
-			//GDetail g = new GDetail(con);
 
 
 			
